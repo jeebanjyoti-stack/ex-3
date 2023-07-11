ac11c9cdb678be8de8487735ea3330e62497110c
jeeban <jeebanjyoti193@gmail.com>
2023-07-11 12:50:24 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exm_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime matchDate = new DateTime(day: 13, month: 07, year: 2023);
+            DateTime currentDate = DateTime.Now;
+            TimeSpan ts = matchDate.Subtract(currentDate);
+            Console.WriteLine("Days Remain " + ts.Days);
+            Console.WriteLine("Days Remain " + ts.Hours);
+            Console.WriteLine(ts.ToString());
+             Console.ReadKey();
+        }
+    }
+}
